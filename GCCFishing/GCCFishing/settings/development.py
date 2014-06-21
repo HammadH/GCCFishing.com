d1dd7cdd0d46a560d75cbfb927a1bef39242df04
Syed Hammad Ahmed <syed@hammad-E531.(none)>
2014-06-21 16:41:00 +0400

    Initial setup

---
+++ GCCFishing/GCCFishing/settings/development.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from .base import *
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
